cc971d0bfb3aaa6ba5fd047683bd53f048787bf8
Kondin Dmitriy <dkondin@gmail.com>
2023-09-22 10:19:49 +0700

    BaseComponent

---
+++ src/routes/sitebill-livewire.php
@@ -0,0 +1,23 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+/*
+Route::group([
+    'namespace' => 'Sitebill\Dragon\app\Http\Controllers\Admin',
+    'prefix' => config('sitebill.dragon.admin_prefix', 'dragon'),
+    'middleware' => ['dragon-cors', 'web'],
+], function () {
+    Route::get('', 'EntityController@index')->name('dragon.entity.index');
+    Route::get('models', 'EntityController@modelsEditor')->name('dragon.entity.models-editor');
+});
+
+Route::group([
+    'namespace' => 'Sitebill\Dragon\app\Http\Controllers\API',
+    'prefix' => config('sitebill.dragon.admin_prefix', 'dragon').'/api',
+    'middleware' => ['dragon-cors', 'web'],
+], function () {
+    Route::get('entity', [\Sitebill\Dragon\app\Http\Controllers\API\EntityController::class, 'test'])->name('dragon.api.entity.test');
+    Route::post('entity', [\Sitebill\Dragon\app\Http\Controllers\API\EntityController::class, 'test'])->name('dragon.api.entity.test');
+    Route::post('entity/{id}', [\Sitebill\Dragon\app\Http\Controllers\API\EntityController::class, 'show'])->name('dragon.api.entity.show');
+});
+*/
